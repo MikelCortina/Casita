75355164bd304a371f427219f476d5ae475071a5
Eneko <eneko_martin@euneiz.com>
2026-03-10 19:24:39 +0100

    Update
    Individual Camera

--- Source/Casita/Public/House/House.h
+++ Source/Casita/Public/House/House.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -10,26 +8,10 @@ UCLASS()
 class CASITA_API AHouse : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	AHouse();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	UPROPERTY(EditAnywhere, Category = "Rotation")
-	FRotator TargetRotation;
-
-	UPROPERTY(EditAnywhere, Category="Rotation")
-	float RotationSpeed = 90.0f;
-
-	void StartRotating();
-
-private:
-	bool bShouldRotate = false;
 };
