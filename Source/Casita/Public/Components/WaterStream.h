2bd451d77b4fe8f065213446ccf7e5205d58e496
mikel <mikel_cortina@euneiz.com>
2026-04-14 18:15:22 +0200

    Water Stream Inicio

---
+++ Source/Casita/Public/Components/WaterStream.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WaterStream.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class CASITA_API AWaterStream : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AWaterStream();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    // Componente que define el área del chorro de agua
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UBoxComponent* TriggerVolume;
+
+    // Componente visual (para que le pongas un material de agua semitransparente o partículas)
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* WaterVisualMesh;
+
+    // Cuánto queremos multiplicar la gravedad mientras esté dentro
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Stream")
+    float GravityMultiplier = 2.0f;
+
+    // Función que se llama cuando el jugador ENTRA al chorro
+    UFUNCTION()
+    void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    // Función que se llama cuando el jugador SALE del chorro
+    UFUNCTION()
+    void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
